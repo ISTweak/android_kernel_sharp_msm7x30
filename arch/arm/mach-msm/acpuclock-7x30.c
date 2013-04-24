767b511665a5027a0d14913ec8359a10b6fd5f44
ISTweak <istweak@d-o-b.org>
2013-04-24 22:59:38 +0900

    2016MHzの有効化

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -136,8 +136,8 @@ static struct clkctl_acpu_speed acpu_freq_tbl[] = {
 	{ 1, 1612800, PLL_2,    3, 0,  UINT_MAX, 1300, VDD_RAW(1300), &pll2_tbl[5]},
 	{ 1, 1708800, PLL_2,    3, 0,  UINT_MAX, 1350, VDD_RAW(1350), &pll2_tbl[6]},
 	{ 1, 1804800, PLL_2,    3, 0,  UINT_MAX, 1400, VDD_RAW(1400), &pll2_tbl[7]},
-	{ 1, 1900800, PLL_2,    3, 0,  UINT_MAX, 1425, VDD_RAW(1450), &pll2_tbl[8]},
-	{ 0, 2016000, PLL_2,    3, 0,  UINT_MAX, 1475, VDD_RAW(1475), &pll2_tbl[9]},
+	{ 1, 1900800, PLL_2,    3, 0,  UINT_MAX, 1450, VDD_RAW(1450), &pll2_tbl[8]},
+	{ 1, 2016000, PLL_2,    3, 0,  UINT_MAX, 1475, VDD_RAW(1475), &pll2_tbl[9]},
 	{ 0 }
 
 };
